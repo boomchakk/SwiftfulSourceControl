156b98f6026e2d8357b7b850d9570b727245adc3
boomchakk <103128249+boomchakk@users.noreply.github.com>
2025-03-07 12:49:04 +1100

    [FEature]  Add hello world text

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var title: String = "dfgdfgdfgd"
+    @State private var title: String = "Hello, world!"
     
     
     var body: some View {
@@ -17,7 +17,7 @@ struct HomeView: View {
             Text("Screen 2!")
             
             Text("Screen 2!")
-            Text("Screen 2!")
+            Text("Screen 3!")
         }
     }
 }
